551a5126a149c9287216a1b04b9c635fb6caf751
mimatache <matache91mh@gmail.com>
2021-11-28 05:43:35 +0200

    moved project from GPL license to Beerware license

--- internal/commands/rootcommand.go
+++ internal/commands/rootcommand.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/mimatache/cyscale/internal/commands/version"
+	"github.com/mimatache/cyscale/internal/commands/about"
 )
 
 // Root is the start of the application. All other commands should either be added to this command, or to a command that is in intself added to this command
@@ -13,10 +13,15 @@ func Root(app string) *cobra.Command {
 	rootCommand := &cobra.Command{
 		Use:   app,
 		Short: fmt.Sprintf("%s is used to find basic security misconfigurations, given the provided input.", app),
+		Long: `
+This is a simple application that parses json files containing information about cloud topologies and performs simple security violation scans.
+This should not be treated as an exhaustive security scan of your cloud environment	
+`,
 	}
 
 	rootCommand.AddCommand(
-		version.Version,
+		about.Version,
+		about.License,
 	)
 
 	return rootCommand
