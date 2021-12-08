291ce90e0124f13a718fc7c90e8775c62398945f
mimatache <matache91mh@gmail.com>
2021-12-08 16:51:36 +0200

    remove relationship list from node

--- internal/graph/node.go
+++ internal/graph/node.go
@@ -8,21 +8,19 @@ import (
 
 func newNode(name, label string, body []byte) Node {
 	return Node{
-		id:            guuid.New().String(),
-		label:         label,
-		name:          name,
-		Body:          body,
-		relationships: []string{},
+		id:    guuid.New().String(),
+		label: label,
+		name:  name,
+		Body:  body,
 	}
 }
 
 // Node represents an item in the graph. It contains the ID of the element, the body and it's relationships to other items
 type Node struct {
-	id            string
-	name          string
-	label         string
-	Body          []byte
-	relationships []string
+	id    string
+	name  string
+	label string
+	Body  []byte
 }
 
 func (n Node) GetID() string {
